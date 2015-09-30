19ba1905ff08e4e0a42f3df110a31f0bc3b958b6
Julian Espinel <julianespinel@gmail.com>
2015-09-30 12:01:39 -0500

    Implement changes to work with Dropwizard 0.8.4.

--- src/main/java/gp/e3/sentinel/infrastructure/healthchecks/MySQLHealthCheck.java
+++ src/main/java/gp/e3/sentinel/infrastructure/healthchecks/MySQLHealthCheck.java
@@ -1,27 +1,23 @@
 package gp.e3.sentinel.infrastructure.healthchecks;
 
+import com.codahale.metrics.health.HealthCheck;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-
-import com.yammer.metrics.core.HealthCheck;
 
 public class MySQLHealthCheck extends HealthCheck {
 	
 	private Connection dbConnection;
 	
-	public MySQLHealthCheck(String name, Connection dbConnection) {
-		
-		super(name);
+	public MySQLHealthCheck(Connection dbConnection) {
 		this.dbConnection = dbConnection;
 	}
 
 	@Override
 	protected Result check() throws Exception {
-		
 		PreparedStatement prepareStatement = dbConnection.prepareStatement("SELECT 1;");
 		ResultSet resultSet = prepareStatement.executeQuery();
-		
 		return resultSet.next()? Result.healthy() : Result.unhealthy("mysql failure.");
 	}
 }
